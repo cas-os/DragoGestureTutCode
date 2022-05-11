fc1a463c6ef71b23b07b0964c9a3e1921881d53f
Kazimierz Osinski <kazimierz.osinski@gmail.com>
2022-05-11 10:02:59 +0200

    /.1

--- DragoGestureTutCode/ContentView.swift
+++ DragoGestureTutCode/ContentView.swift
@@ -8,9 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var offset: CGSize = .zero
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        
+        ZStack {
+            
+            VStack {
+                Text("\(offset.width)")
+                Spacer()
+            }
+            
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: 300, height: 500)
+                .foregroundColor(.orange)
+                .offset(offset)
+                .scaleEffect(getScaleAmount())
+                .rotationEffect(Angle(degrees: getRotationAmount()))
+                .gesture(DragGesture()
+                            .onChanged({ value in
+                                withAnimation(.spring()){
+                                    offset = value.translation
+                                }
+                            })
+                            .onEnded({ value in
+                                withAnimation(.spring()){
+                                    offset = .zero
+                                }
+                            })
+            )
+        }
+    }
+    
+    func getScaleAmount() -> CGFloat {
+        let max = UIScreen.main.bounds.width / 2
+        let currentAmount = abs(offset.width)
+        let percentage = currentAmount / max
+        
+        
+        return 1.0 - min(percentage, 0.5) * 0.5
+    }
+    
+    func getRotationAmount() -> Double {
+        let max = UIScreen.main.bounds.width / 2
+        let currentAmount = offset.width
+        let percentage = currentAmount / max
+        let percentageAsDouble = Double(percentage)
+        let maxAngle: Double = 10
+        
+        return percentageAsDouble * maxAngle
     }
 }
 
