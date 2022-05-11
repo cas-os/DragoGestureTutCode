41d2e3f780475bbbb20573e20385af9e5654a3f6
Kazimierz Osinski <kazimierz.osinski@gmail.com>
2022-05-11 08:02:18 +0200

    Initial Commit

---
+++ DragoGestureTutCode/DragoGestureTutCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragoGestureTutCodeApp.swift
+//  DragoGestureTutCode
+//
+//  Created by HAL-9001 on 11/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct DragoGestureTutCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
